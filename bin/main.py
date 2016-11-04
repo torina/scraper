cd5d367e2abd36bb9fbfc1f108d7ef5fce18537f
Victoria Bula <viktoriia.bula@windeln.de>
2016-11-04 21:30:24 +0100

    f

--- bin/main.py
+++ bin/main.py
@@ -2,7 +2,7 @@ from urllib.parse import urljoin
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 from selenium import webdriver
-import os
+import os, csv
 
 chromedriver = "C:/Program Files/chrome/chromedriver.exe"
 os.environ["webdriver.chrome.driver"] = chromedriver
@@ -13,21 +13,28 @@ wiki = "http://www.starz.com/movies"
 driver.get(wiki)
 html = driver.page_source
 soup = BeautifulSoup(html, "html.parser")
+lobbying = {}
 
 for mvSlice in soup.find_all("a", class_="slick-link"):
     href = (mvSlice['href'])
     movieUrl = urljoin(wiki, href)
-    print(movieUrl)
+    #print(movieUrl)
     driver.get(movieUrl)
     html = driver.page_source
     soup = BeautifulSoup(html, "html.parser")
     pretty = soup.prettify()
 
     title = soup.find_all("div", class_="title")[0]
-    print(title.find_all("h1")[0].text)
+    #print(title.find_all("h1")[0].text)
+    lobbying[title.find_all("h1")[0].text] = {} #create new dict
 
-#make a wrapper
     rating = soup.find_all("li", {"ng-if": "vm.details.mpaaRating"})
-    print(rating[0].text)
+    #print(rating[0].text)
+    # error here
+    if(rating[0] != None):
+        lobbying[title.find_all("h1")[0].text]["rate"] = rating[0].text
+
+for item in lobbying.keys():
+    print(item + ": " + "\n\t" + "rate: " + lobbying[item]["rate"] + "\n\n")
 
 driver.quit()
