3bd5a3496f6df1a6da88e5e6afbe5d95ff7db8b2
Julian Koberg <Kokweazel@Julians-MBP.fritz.box>
2016-11-15 21:01:06 +0100

    Trying to get driver up

--- bin/main.py
+++ bin/main.py
@@ -1,40 +1,77 @@
-from urllib.parse import urljoin
-from urllib.request import urlopen
-from bs4 import BeautifulSoup
-from selenium import webdriver
-import os, csv
+import scraper as scr
 
-chromedriver = "C:/Program Files/chrome/chromedriver.exe"
-os.environ["webdriver.chrome.driver"] = chromedriver
-driver = webdriver.Chrome(chromedriver)
-#todo remove
+# import urllib2
 
-wiki = "http://www.starz.com/movies"
-driver.get(wiki)
-html = driver.page_source
-soup = BeautifulSoup(html, "html.parser")
-lobbying = {}
+# from urllib.parse import urljoin
+# from urllib.request import urlopen
 
-for mvSlice in soup.find_all("a", class_="slick-link"):
-    href = (mvSlice['href'])
-    movieUrl = urljoin(wiki, href)
-    #print(movieUrl)
-    driver.get(movieUrl)
-    html = driver.page_source
-    soup = BeautifulSoup(html, "html.parser")
-    pretty = soup.prettify()
+# from bs4 import BeautifulSoup
 
-    title = soup.find_all("div", class_="title")[0]
-    #print(title.find_all("h1")[0].text)
-    lobbying[title.find_all("h1")[0].text] = {} #create new dict
+WEBPAGEURL = "http://www.starz.com/movies/26915"
+DRIVERLOCATION = "/Users/Kokweazel/scraper/chromedriver"
 
-    rating = soup.find_all("li", {"ng-if": "vm.details.mpaaRating"})
-    #print(rating[0].text)
-    # error here
-    if(rating[0] != None):
-        lobbying[title.find_all("h1")[0].text]["rate"] = rating[0].text
+# print con.__dict__
 
-for item in lobbying.keys():
-    print(item + ": " + "\n\t" + "rate: " + lobbying[item]["rate"] + "\n\n")
+if __name__ == '__main__':
+    with scr.SeleniumConnection(DRIVERLOCATION) as connection:
 
-driver.quit()
+        html = connection(WEBPAGEURL)
+
+        print html
+
+
+
+
+
+## REFERENCE CODE ##
+
+# driver = webdriver.Chrome()
+# driver.get(wiki)
+
+# req = urllib2.Request(wiki)
+# response = urllib2.urlopen(req)
+# html = response.read()
+
+# print 'Set 30 years after Return' in str(html)
+
+# soup = BeautifulSoup(str(html), "html.parser")
+# soup = soup.prettify()
+# print 'Set 30 years after Return' in soup
+# lobbying = {}
+
+# print soup.find_all("a", class_="slick-link")
+
+# for mvSlice in soup.find_all("a", class_="slick-link"):
+#     href = (mvSlice['href'])
+#     print wiki, href
+    # movieUrl = urljoin(wiki, href)
+#     driver.get(movieUrl)
+#     html = driver.page_source
+#     soup = BeautifulSoup(html, "html.parser")
+#     pretty = soup.prettify()
+
+# title = soup.find_all(True)
+# for tag in title:
+#     if tag.has_attr('data-ng-controller'):
+#         break
+
+# section = tag.find_all("section")[0]
+
+# subsection = section.find_all("div")[0]
+
+# print soup.prettify()
+
+# #     #print(title.find_all("h1")[0].text)
+# #     lobbying[title.find_all("h1")[0].text] = {} #create new dict
+
+# # print soup.find_all("section", class_="block-details")
+
+# rating = soup.find_all("li", {"ng-if": "vm.details.mpaaRating"})
+# print rating
+#     #print(rating[0].text)
+#     # error here
+#     if(rating[0] != None):
+#         lobbying[title.find_all("h1")[0].text]["rate"] = rating[0].text
+
+# for item in lobbying.keys():
+#     print(item + ": " + "\n\t" + "rate: " + lobbying[item]["rate"] + "\n\n")
---
+++ bin/scraper.py
@@ -0,0 +1,35 @@
+import os
+
+from selenium import webdriver
+from bs4 import BeautifulSoup
+
+class SeleniumConnection:
+    browser = "webdriver.chrome.driver"
+
+    def __init__(self, driverlocation, set_env=False):
+        if set_env:
+            os.environ[self.browser] = driverlocation
+
+        self.driverlocation = driverlocation
+
+    def __enter__(self):
+        return webdriver.Chrome(self.driverlocation)
+
+    def __call__(self, url):
+        self.driver.get(url)
+
+        return self.driver.page_source
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if exc_type:
+            print exc_type
+
+        self.driver.quit()
+
+class Soup:
+    parser = "html.parser"
+    def __init__(self, html):
+        self._soup = BeautifulSoup(html, self.parser)
+
+    def printout(self):
+        print self._soup.prettify()
