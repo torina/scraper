f63099bd54bb426b16fb7eb77467b9305c048cca
Victoria Bula <viktoriia.bula@windeln.de>
2016-11-18 22:00:37 +0100

    third commit

--- bin/Movie.py
+++ bin/Movie.py
@@ -5,17 +5,20 @@ class Movie:
                  rating,
                  time,
                  genre,
-                 year):
-                 # synopsis,
-                 # castAndCrew
+                 year,
+                 synopsis
+    #            castAndCrew
+    ):
         self.title = title
         self.rating = rating
         self.time = time
         self.genre = genre
         self.year = year
+        self.synopsis = synopsis
 
     def __str__(self):
         if not self.title or not self.rating:
             return "ERROR WITH MOVIE"
 
-        return "Name: " + self.title.text + " -- Rating: " + self.rating.text + " -- Genre: " +  self.genre.text +" Year -- "+self.year.text
+        return "Name: " + self.title.text + " -- Rating: " + self.rating.text + " -- Genre: " +  self.genre.text +\
+               "  -- Year: " +self.year.text + " -- Synopsis: " + self.synopsis.text
--- bin/main.py
+++ bin/main.py
@@ -1,16 +1,21 @@
 from bs4 import BeautifulSoup
 import WebNavigator
 import MovieFabric
-
+# configs
+# http://selenium-python.readthedocs.io/api.html#module-selenium.webdriver.remote.webelement
 baseURL = "http://www.starz.com/movies"
 
-pathTemplate = '//ul[@class="meta-list"]/li[{0}]'
+#setting XPathes for every elem.
 titleXpath = '//div[@class="title"]/h1'
+
+pathTemplate = '//ul[@class="meta-list"]/li[{0}]'
 ratingxPath = pathTemplate.format(2)
 timeXpath = pathTemplate.format(3)
 genreXpath = pathTemplate.format(4)
 yearXpath = pathTemplate.format(5)
+castCrew = '//div[@class="block-description"]'
 
+# end config
 browser = WebNavigator.Browser(baseURL)
 html = browser()
 
@@ -26,7 +31,7 @@ for mvSlice in soup.find_all("a", class_="slick-link"):
     soup = BeautifulSoup(movieHtml, "html.parser")
     pretty = soup.prettify()
 
-    # caseCrew = browser.getByXpath('/h2[@class="slider-title"]')
-    movie = factory.produceMovie(titleXpath, ratingxPath, timeXpath, genreXpath, yearXpath)
+
+    movie = factory.produceMovie(titleXpath, ratingxPath, timeXpath, genreXpath, yearXpath, castCrew)
     print(str(movie))
 browser.tearown()
