873abe8c3a6b2d49a05c55bad7b04610bcb7d086
Victoria Bula <viktoriia.bula@windeln.de>
2016-11-18 21:47:56 +0100

    second commit

---
+++ bin/WebNavigator.py
@@ -0,0 +1,27 @@
+from urllib.parse import urljoin
+from selenium import webdriver
+
+## The Browser Class handles browsing the Web. __call__ returns the sourcecode
+class Browser:
+    def __init__(self, baseurl):
+        self.baseurl = baseurl
+        self.driver = webdriver.Chrome()
+
+    def __call__(self, path=""):
+        # Add the Optional Path to the BaseUrl
+        url = urljoin(self.baseurl, path)
+        # Load the site
+        self.driver.get(url)
+        # Return the SourceCode
+        return self.driver.page_source
+
+    def tearown(self):
+        # Clean Up
+        self.driver.close()
+
+        if self.driver:
+            print("\nCan't get rid of that shtty ErrorMsg:")
+            self.driver.quit()
+
+    def getByXpath(self, xPath):
+        return self.driver.find_element_by_xpath(xPath)
--- bin/main.py
+++ bin/main.py
@@ -1,40 +1,32 @@
-from urllib.parse import urljoin
-from urllib.request import urlopen
 from bs4 import BeautifulSoup
-from selenium import webdriver
-import os, csv
+import WebNavigator
+import MovieFabric
 
-chromedriver = "C:/Program Files/chrome/chromedriver.exe"
-os.environ["webdriver.chrome.driver"] = chromedriver
-driver = webdriver.Chrome(chromedriver)
-#todo remove
+baseURL = "http://www.starz.com/movies"
 
-wiki = "http://www.starz.com/movies"
-driver.get(wiki)
-html = driver.page_source
+pathTemplate = '//ul[@class="meta-list"]/li[{0}]'
+titleXpath = '//div[@class="title"]/h1'
+ratingxPath = pathTemplate.format(2)
+timeXpath = pathTemplate.format(3)
+genreXpath = pathTemplate.format(4)
+yearXpath = pathTemplate.format(5)
+
+browser = WebNavigator.Browser(baseURL)
+html = browser()
+
 soup = BeautifulSoup(html, "html.parser")
-lobbying = {}
+results = {}
 
+factory = MovieFabric.MovieFabric(browser)
+# for [presumably] all hrefs
 for mvSlice in soup.find_all("a", class_="slick-link"):
     href = (mvSlice['href'])
-    movieUrl = urljoin(wiki, href)
-    #print(movieUrl)
-    driver.get(movieUrl)
-    html = driver.page_source
-    soup = BeautifulSoup(html, "html.parser")
+    # todo how to move through pages
+    movieHtml = browser(href)
+    soup = BeautifulSoup(movieHtml, "html.parser")
     pretty = soup.prettify()
 
-    title = soup.find_all("div", class_="title")[0]
-    #print(title.find_all("h1")[0].text)
-    lobbying[title.find_all("h1")[0].text] = {} #create new dict
-
-    rating = soup.find_all("li", {"ng-if": "vm.details.mpaaRating"})
-    #print(rating[0].text)
-    # error here
-    if(rating[0] != None):
-        lobbying[title.find_all("h1")[0].text]["rate"] = rating[0].text
-
-for item in lobbying.keys():
-    print(item + ": " + "\n\t" + "rate: " + lobbying[item]["rate"] + "\n\n")
-
-driver.quit()
+    # caseCrew = browser.getByXpath('/h2[@class="slider-title"]')
+    movie = factory.produceMovie(titleXpath, ratingxPath, timeXpath, genreXpath, yearXpath)
+    print(str(movie))
+browser.tearown()
